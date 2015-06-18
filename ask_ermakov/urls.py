1391ab1b13bb5f7e9f33b08f3a956a2935d43b68
iHelos <ihelos.ermakov@gmail.com>
2015-06-18 23:26:28 +0300

    login added

--- ask_ermakov/urls.py
+++ ask_ermakov/urls.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^ask$', ask, name='ask'),
     url(r'^signup$', register, name='register'),
+    url(r'^logout$', 'django.contrib.auth.views.logout',  {'next_page': '/'}),
+    url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'user/login.html', 'redirect_field_name': '/question'}),
 )
